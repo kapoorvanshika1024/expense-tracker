2c826a72bcd569f5f83b87830ac670230e6258f4
kapoorvanshika1024 <kapoorvanshika1024@gmail.com>
2024-09-30 23:01:02 -0400

    Second commit

---
+++ app/(routes)/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { UserButton } from '@clerk/nextjs'
+function Dashboard() {
+  return (
+    <div>
+      DashBoard
+    </div>
+  )
+}
+
+export default Dashboard
